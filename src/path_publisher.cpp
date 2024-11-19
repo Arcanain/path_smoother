4dcd084277e5d8a6cf1acf529c2fb9ecaef27363
tomtomApp <mtom0809tom0809@gmail.com>
2024-11-19 15:43:18 +0900

    [add]emcl_sub

--- src/path_publisher.cpp
+++ src/path_publisher.cpp
@@ -17,7 +17,7 @@ public:
         // ホームディレクトリのパスを取得
         std::string home_dir = getenv("HOME");
         // ファイルのフルパスを組み立てる
-        std::string file_path = home_dir + "/ros2_ws/src/path_smoother/path/tsukuba_1109_complement.csv";
+        std::string file_path = home_dir + "/ros2_ws/src/path_smoother/path/pedl_11_19.csv";
         loadPathData(file_path);
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(100), std::bind(&PathPublisher::publishPath, this));
